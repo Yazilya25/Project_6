2812b5bb966e5798b8e76bdc23d853c4602fc615
Абдуллина Язиля <A_250494@mail.ru>
2022-05-02 23:18:56 +0300

    Одновременный вывод результатов

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
-    public float calculate(double sum, double period, double percent) {
-        double payment=(((percent / 1200 * (Math.pow((1 + percent / 1200), period))) / (Math.pow((1 + percent / 1200), period) - 1)) * sum);
-        return (float) payment;
+    public double calculate(double sum, double period, double percent) {
+        double payment = (((percent / 1200 * (Math.pow((1 + percent / 1200), period))) / (Math.pow((1 + percent / 1200), period) - 1)) * sum);
+        return payment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        // System.out.println("Введите сумму кредита ");
-        double sum = 1_000_000;
-        //System.out.println("Введите срок кредита ");
-        double period = 36;
-        //System.out.println("Введите %-ую ставку кредита ");
-        double percent = 9.99;
-        double payment = service.calculate(sum, period, percent);
-        System.out.println("Сумма ежемесячного платежа: " + payment);
+
+        double payment1 = service.calculate(1_000_000, 12, 9.99);
+        System.out.println("Сумма ежемесячного платежа: " + payment1 + " руб.");
+
+        double payment2 = service.calculate(1_000_000, 24, 9.99);
+        System.out.println("Сумма ежемесячного платежа: " + payment2 + " руб.");
+
+        double payment3 = service.calculate(1_000_000, 36, 9.99);
+        System.out.println("Сумма ежемесячного платежа: " + payment3 + " руб.");
     }
 }
